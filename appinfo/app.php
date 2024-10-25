d08d09156637d66e0afd453f90f9996ecb922f3e
dotcf <92203938+dotcfg@users.noreply.github.com>
2024-10-25 23:17:32 +0200

    Update app.php

--- appinfo/app.php
+++ appinfo/app.php
@@ -3,7 +3,7 @@
  * Load Javascript
  */
 use OCP\Util;
-$eventDispatcher = OC::$server->get(\OCP\EventDispatcher\IEventDispatcher::class),
+$eventDispatcher = \OC::$server->get(\OCP\EventDispatcher\IEventDispatcher::class),
 $eventDispatcher->addListener('OCA\Files::loadAdditionalScripts', function(){
     Util::addScript('video_converter', 'conversion' );
     Util::addStyle('video_converter', 'style' );
